5c4bfc9248cdfe45dd922d7d7dc286405c921601
Louis-Kaile <132564543+Louis-Kaile@users.noreply.github.com>
2026-04-10 20:26:55 +0100

    analytics tag

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -5,7 +5,7 @@ import type { Metadata } from "next";
 import { draftMode } from "next/headers";
 import { VisualEditing, toPlainText } from "next-sanity";
 import { Toaster } from "sonner";
-import Script from "next/script";
+import { GoogleAnalytics, GoogleTagManager } from "@next/third-parties/google";
 import BodyClassUpdater from "@/app/components/BodyClassUpdater";
 // Site credit removed
 import TouchTapEffect from "@/app/components/TouchTapEffect";
@@ -74,26 +74,21 @@ export default async function RootLayout({
     <html lang="en">
       <body>
         <BodyClassUpdater />
-        {process.env.NEXT_PUBLIC_GTM_CONTAINER_ID && (
-          <noscript dangerouslySetInnerHTML={{ __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GTM_CONTAINER_ID}" height="0" width="0" style="display:none;visibility:hidden"></iframe>` }} />
-        )}
 
         <section className="site-wrapper">
 
           {/* The <Toaster> component is responsible for rendering toast notifications used in /app/client-utils.ts and /app/components/DraftModeToast.tsx */}
-          {/* Toaster and analytics scripts */}
           <Toaster />
-          {/* If GTM is configured, inject GTM script + noscript iframe; else fall back to direct GA */}
-          {process.env.NEXT_PUBLIC_GTM_CONTAINER_ID ? (
+          {process.env.NODE_ENV === 'production' && (
             <>
-              <Script id="gtm-script" strategy="afterInteractive" dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'? '&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GTM_CONTAINER_ID}');` }} />
+              {process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID && (
+                <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID} />
+              )}
+              {process.env.NEXT_PUBLIC_GTM_CONTAINER_ID && (
+                <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GTM_CONTAINER_ID} />
+              )}
             </>
-          ) : process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID ? (
-            <>
-              <Script src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`} strategy="afterInteractive" />
-              <Script id="ga-init" strategy="afterInteractive" dangerouslySetInnerHTML={{ __html: `window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');` }} />
-            </>
-          ) : null}
+          )}
           {isDraftMode && (
             <>
               <DraftModeToast />
